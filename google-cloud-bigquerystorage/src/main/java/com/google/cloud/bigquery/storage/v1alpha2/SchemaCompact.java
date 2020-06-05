70a1369dd05be00f366d7dff4d6a10fc08b365cb
Allen Chen <allenc3@illinois.edu>
2020-06-05 20:13:45 +0000

    Reformatted to pass checkstyle.

--- google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1alpha2/SchemaCompact.java
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1alpha2/SchemaCompact.java
@@ -22,8 +22,6 @@ import com.google.cloud.bigquery.TableId;
 import com.google.cloud.bigquery.testing.RemoteBigQueryHelper;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.protobuf.Descriptors;
-import com.google.zetasql.ProtoType;
-import com.google.zetasql.TypeAnnotationProto;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
