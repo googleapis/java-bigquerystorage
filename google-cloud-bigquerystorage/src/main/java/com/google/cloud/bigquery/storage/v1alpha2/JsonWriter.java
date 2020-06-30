b83893513456f4a71cb039a325fbb12b056a7a11
allenc3 <allenc3@illinois.edu>
2020-06-30 15:21:21 -0500

    Built up basic version of JsonWriter that holds a Table.TableSchema and StreamWriter, and exposes the append() function from the StreamWriter

--- google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1alpha2/JsonWriter.java
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1alpha2/JsonWriter.java
@@ -16,51 +16,71 @@
 package com.google.cloud.bigquery.storage.v1alpha2;
 
 import com.google.api.core.*;
+import com.google.cloud.bigquery.storage.v1alpha2.ProtoBufProto.ProtoRows;
+import com.google.cloud.bigquery.storage.v1alpha2.Storage.AppendRowsRequest;
+import com.google.cloud.bigquery.storage.v1alpha2.Storage.AppendRowsResponse;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
+import com.google.protobuf.Int64Value;
+import com.google.protobuf.Message;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class JsonWriter {
-  // private static final Logger LOG = Logger.getLogger(JsonWriter.class.getName());
-  // private Table.TableSchema BQTableSchema;
-  // private StreamWriter streamWriter;
-  // private Descriptor descriptor;
-  //
+  private static final Logger LOG = Logger.getLogger(JsonWriter.class.getName());
+  private Table.TableSchema BQTableSchema;
+  private StreamWriter streamWriter;
+  private Descriptors.Descriptor descriptor;
+
   // public JsonWriter(StreamWriter.Builder builder) {
   //   this.streamWriter = builder.build();
-  //   this.BQSchema = streamWriter.getTableSchema();
-  //   descriptor = JsonToProtoConverter.BQSchemaToProtoSchema(BQSchema);
+  //   this.BQTableSchema = streamWriter.getTableSchema();
+  //   descriptor = JsonToProtoConverter.BQTableSchemaToProtoSchema(BQTableSchema);
   // }
-  //
-  // public JsonWriter(StreamWriter streamWriter) {
-  //   this.streamWriter = streamWriter;
-  //   this.BQSchema = streamWriter.getTableSchema();
-  //   descriptor = JsonToProtoConverter.BQSchemaToProtoSchema(BQSchema);
-  // }
-  //
-  // public ApiFuture<AppendRowsResponse> append(JSONArray jsonRows) {
-  //   List<DynamicMessage> protoRows = new ArrayList<DynamicMessage>();
-  //   for (int i = 0; i < jsonRows.size(); i++) {
-  //     JSONObject jsonObject = jsonRows.get(i);
-  //     protoRows.add(JsonToProtoConverter.protoSchemaToProtoMessage(descriptor, jsonObject));
-  //   }
-  //
-  //   ProtoRows.Builder rowsBuilder = ProtoRows.newBuilder();
-  //   Descriptors.Descriptor descriptor = null;
-  //   for (Message protoRow : protoRows) {
-  //     rowsBuilder.addSerializedRows(protoRow.toByteString());
-  //   }
-  //
-  //   AppendRowsRequest.ProtoData.Builder data = AppendRowsRequest.ProtoData.newBuilder();
-  //   data.setWriterSchema(ProtoSchemaConverter.convert(protoRows.get(0).getDescriptorForType()));
-  //   data.setRows(rowsBuilder.build());
-  //
-  //   return ApiFutures.<Storage.AppendRowsResponse, Long>transform(
-  //       streamWriter.append(AppendRowsRequest.newBuilder().setProtoRows(data.build()).build()),
-  //       new ApiFunction<Storage.AppendRowsResponse, Long>() {
-  //         @Override
-  //         public Long apply(Storage.AppendRowsResponse appendRowsResponse) {
-  //           return Long.valueOf(appendRowsResponse.getOffset());
-  //         }
-  //       },
-  //       MoreExecutors.directExecutor());
-  //
-  // }
+
+  public JsonWriter(StreamWriter streamWriter) throws Descriptors.DescriptorValidationException {
+    this.streamWriter = streamWriter;
+    this.BQTableSchema = streamWriter.getTableSchema();
+    descriptor = JsonToProtoConverter.BQTableSchemaToProtoSchema(BQTableSchema);
+  }
+
+  public ApiFuture<AppendRowsResponse> append(JSONArray jsonRows, long offset)
+      throws IllegalArgumentException {
+    List<DynamicMessage> protoRows = new ArrayList<DynamicMessage>();
+    for (int i = 0; i < jsonRows.length(); i++) {
+      java.lang.Object jsonObject = jsonRows.get(i);
+      if (jsonObject instanceof JSONObject) {
+        protoRows.add(
+            JsonToProtoConverter.protoSchemaToProtoMessage(descriptor, (JSONObject) jsonObject));
+      } else {
+        throw new IllegalArgumentException(
+            "Illegal JSON Formatting: JSON input is not a JSONArray of JSONObjects.");
+      }
+    }
+
+    ProtoRows.Builder rowsBuilder = ProtoRows.newBuilder();
+    Descriptors.Descriptor descriptor = null;
+    for (Message protoRow : protoRows) {
+      rowsBuilder.addSerializedRows(protoRow.toByteString());
+    }
+
+    AppendRowsRequest.ProtoData.Builder data = AppendRowsRequest.ProtoData.newBuilder();
+    data.setWriterSchema(ProtoSchemaConverter.convert(protoRows.get(0).getDescriptorForType()));
+    data.setRows(rowsBuilder.build());
+    if (offset > 0) {
+      return streamWriter.append(
+          AppendRowsRequest.newBuilder()
+              .setProtoRows(data.build())
+              .setOffset(Int64Value.of(offset))
+              .build());
+    }
+    return streamWriter.append(AppendRowsRequest.newBuilder().setProtoRows(data.build()).build());
+  }
+
+  public ApiFuture<AppendRowsResponse> append(JSONArray jsonRows) throws IllegalArgumentException {
+    return append(jsonRows, -1);
+  }
 }
