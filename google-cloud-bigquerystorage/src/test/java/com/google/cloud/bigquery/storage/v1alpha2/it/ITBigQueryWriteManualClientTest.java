b10739f586d9cfefad97838c732d970abe6290ec
allenc3 <allenc3@illinois.edu>
2020-08-13 12:00:37 -0500

    Add IT test

--- google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1alpha2/it/ITBigQueryWriteManualClientTest.java
+++ google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1alpha2/it/ITBigQueryWriteManualClientTest.java
@@ -605,12 +605,43 @@ public class ITBigQueryWriteManualClientTest {
       assertTrue(expectedOffset.remove(response.get()));
     }
     assertTrue(expectedOffset.isEmpty());
+
+    JSONObject m1_json = new JSONObject();
+    a_json.put("foo", "aaa");
+    JSONObject m2_json = new JSONObject();
+    b_json.put("foo", "bbb");
+    final JSONArray jsonArr = new JSONArray();
+    jsonArr.put(a_json);
+    jsonArr.put(b_json);
+
+    expectedOffset = new HashSet<>();
+    for (int i = 0; i < 10; i++) {
+      expectedOffset.add(Long.valueOf(i * 2));
+    }
+    executor = Executors.newFixedThreadPool(10);
+    responses = new ArrayList<>();
+    callable =
+        new Callable<Long>() {
+          @Override
+          public Long call() throws IOException, InterruptedException, ExecutionException {
+            ApiFuture<Long> result = DirectWriter.append(tableId, jsonArr);
+            return result.get();
+          }
+        };
+    for (int i = 0; i < 10; i++) {
+      responses.add(executor.submit(callable));
+    }
+    for (Future<Long> response : responses) {
+      assertTrue(expectedOffset.remove(response.get()));
+    }
+    assertTrue(expectedOffset.isEmpty());
     executor.shutdown();
     try {
       executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
     } catch (InterruptedException e) {
       LOG.info(e.toString());
     }
+
     DirectWriter.clearCache();
   }
 
