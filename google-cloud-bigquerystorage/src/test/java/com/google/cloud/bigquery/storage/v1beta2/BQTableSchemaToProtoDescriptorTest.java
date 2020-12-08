05a14289602ab33f452ec54044315f640c74d96f
yirutang <yiru@google.com>
2020-12-08 10:47:00 -0800

    fix the messed headers

--- google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta2/BQTableSchemaToProtoDescriptorTest.java
+++ google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta2/BQTableSchemaToProtoDescriptorTest.java
@@ -1,10 +1,11 @@
 /*
  * Copyright 2020 Google LLC
-public class BQTableSchemaToProtoDescriptorTest {}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +16,6 @@ public class BQTableSchemaToProtoDescriptorTest {}
 package com.google.cloud.bigquery.storage.v1beta2;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
 
 import com.google.cloud.bigquery.storage.test.JsonTest.*;
 import com.google.cloud.bigquery.storage.test.SchemaTest.*;
