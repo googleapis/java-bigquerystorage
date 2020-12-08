80ee6427a818f485976c20bc5fffae9ac43f1378
yirutang <yiru@google.com>
2020-12-07 16:34:26 -0800

    feat: v1beta2 manual client library for BigQueryWrite

---
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1beta2/SchemaCompatibility.java
@@ -0,0 +1,4 @@
+package com.google.cloud.bigquery.storage.v1beta2;
+
+public class SchemaCompatibility {
+}
---
+++ google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta2/it/ITBigQueryWriteManualClientTest.java
@@ -0,0 +1,4 @@
+package com.google.cloud.bigquery.storage.v1beta2.it;
+
+public class ITBigQueryWriteManualClientTest {
+}
