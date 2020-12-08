13c718c96bb1f7bab4290ef737fe2fe09197a3f0
Yiru Tang <yiru@google.com>
2020-12-07 16:52:00 -0800

    Apply suggestions from code review
    Co-authored-by: yoshi-code-bot <70984784+yoshi-code-bot@users.noreply.github.com>

--- google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1beta2/ProtoSchemaConverter.java
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1beta2/ProtoSchemaConverter.java
@@ -1,7 +1,6 @@
 /*
  * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+public class ProtoSchemaConverter {}
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
