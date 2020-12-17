233f880ac38ae03e025b2df756395daea9bb85bf
yoshi-automation <yoshi-automation@google.com>
2020-12-17 15:04:39 -0800

    fix: Update gapic-generator-java to 0.0.7
    Committer: @miraleung
    PiperOrigin-RevId: 345476969
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Dec 3 10:07:32 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 7be2c821dd88109038c55c89f7dd48f092eeab9d
    Source-Link: https://github.com/googleapis/googleapis/commit/7be2c821dd88109038c55c89f7dd48f092eeab9d

--- proto-google-cloud-bigquerystorage-v1/src/main/java/com/google/cloud/bigquery/storage/v1/ReadStreamName.java
+++ proto-google-cloud-bigquerystorage-v1/src/main/java/com/google/cloud/bigquery/storage/v1/ReadStreamName.java
@@ -121,7 +121,7 @@ public class ReadStreamName implements ResourceName {
   public static List<String> toStringList(List<ReadStreamName> values) {
     List<String> list = new ArrayList<>(values.size());
     for (ReadStreamName value : values) {
-      if (Objects.isNull(value)) {
+      if (value == null) {
         list.add("");
       } else {
         list.add(value.toString());
@@ -136,20 +136,20 @@ public class ReadStreamName implements ResourceName {
 
   @Override
   public Map<String, String> getFieldValuesMap() {
-    if (Objects.isNull(fieldValuesMap)) {
+    if (fieldValuesMap == null) {
       synchronized (this) {
-        if (Objects.isNull(fieldValuesMap)) {
+        if (fieldValuesMap == null) {
           ImmutableMap.Builder<String, String> fieldMapBuilder = ImmutableMap.builder();
-          if (!Objects.isNull(project)) {
+          if (project != null) {
             fieldMapBuilder.put("project", project);
           }
-          if (!Objects.isNull(location)) {
+          if (location != null) {
             fieldMapBuilder.put("location", location);
           }
-          if (!Objects.isNull(session)) {
+          if (session != null) {
             fieldMapBuilder.put("session", session);
           }
-          if (!Objects.isNull(stream)) {
+          if (stream != null) {
             fieldMapBuilder.put("stream", stream);
           }
           fieldValuesMap = fieldMapBuilder.build();
