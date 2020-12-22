01e46dfb282e2b59eb906c56b6f9c8b3de1dc426
yirutang <yiru@google.com>
2020-12-22 13:33:53 -0800

    .

--- google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta2/JsonStreamWriterTest.java
+++ google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta2/JsonStreamWriterTest.java
@@ -934,7 +934,7 @@ public class JsonStreamWriterTest {
                   .build());
         }
 
-        offsetSets.add((long) i);
+        offset_sets.add((long) i);
         Thread t =
             new Thread(
                 new Runnable() {
@@ -956,7 +956,7 @@ public class JsonStreamWriterTest {
       for (int i = 0; i < numberThreads; i++) {
         thread_arr[i].join();
       }
-      assertTrue(offsetSets.size() == 0);
+      assertTrue(offset_sets.size() == 0);
       for (int i = 0; i < numberThreads; i++) {
         assertEquals(
             1,
@@ -1020,7 +1020,7 @@ public class JsonStreamWriterTest {
       for (int i = 0; i < numberThreads; i++) {
         thread_arr[i].join();
       }
-      assertTrue(offsetSets.size() == 0);
+      assertTrue(offset_sets.size() == 0);
       for (int i = 0; i < numberThreads; i++) {
         assertEquals(
             1,
