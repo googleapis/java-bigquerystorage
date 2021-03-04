d38599502bc4cffc0048d031b9224222375fb735
JacobStocklass <jstocklass@google.com>
2021-03-04 23:00:20 +0000

    Refactoring down to only the simple case. Complex case will be handled in a different PR

--- google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta2/st/ITBigQueryStorageLongRunningWriteTest.java
+++ google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta2/st/ITBigQueryStorageLongRunningWriteTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@ import com.google.cloud.ServiceOptions;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.DatasetInfo;
 import com.google.cloud.bigquery.Field;
-import com.google.cloud.bigquery.Field.Mode;
 import com.google.cloud.bigquery.FieldValueList;
 import com.google.cloud.bigquery.Schema;
 import com.google.cloud.bigquery.StandardSQLTypeName;
@@ -60,10 +59,9 @@ public class ITBigQueryStorageLongRunningWriteTest {
       Logger.getLogger(ITBigQueryStorageLongRunningTest.class.getName());
   private static final String LONG_TESTS_ENABLED_PROPERTY =
       "bigquery.storage.enable_long_running_tests";
-
-  private static String dataset;
   private static final String DESCRIPTION = "BigQuery Write Java long test dataset";
 
+  private static String dataset;
   private static BigQueryWriteClient client;
   private static String parentProjectId;
   private static BigQuery bigquery;
@@ -79,69 +77,7 @@ public class ITBigQueryStorageLongRunningWriteTest {
         object.put("test_datetime", String.valueOf(LocalDateTime.now()));
         break;
       case COMPLEX:
-        object.put("test_str", "aaa");
-        object.put(
-            "test_numerics1",
-            new JSONArray(
-                new String[] {
-                  "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
-                  "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
-                  "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41",
-                  "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54",
-                  "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67",
-                  "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80",
-                  "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93",
-                  "94", "95", "96", "97", "98", "99", "100"
-                }));
-        object.put(
-            "test_numerics2",
-            new JSONArray(
-                new String[] {
-                  "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
-                  "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
-                  "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41",
-                  "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54",
-                  "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67",
-                  "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80",
-                  "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93",
-                  "94", "95", "96", "97", "98", "99", "100"
-                }));
-        object.put(
-            "test_numerics3",
-            new JSONArray(
-                new String[] {
-                  "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
-                  "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
-                  "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41",
-                  "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54",
-                  "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67",
-                  "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80",
-                  "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93",
-                  "94", "95", "96", "97", "98", "99", "100"
-                }));
-        object.put("test_datetime", String.valueOf(LocalDateTime.now()));
-        object.put(
-            "test_bools",
-            new JSONArray(
-                new boolean[] {
-                  false, true, false, true, false, true, false, true, false, true, false, true,
-                  false, true, false, true, false, true, true, false, true, false, true, false,
-                  true, false, true, false, true, false, true, true, false, true, false, true,
-                  false, true, false, true, false, true, false, true, true, false, true, false,
-                  true, false, true, false, true, false, true, false, true, true, false, true,
-                  false, true, false, true, false, true, false, true, false, true, true, false,
-                  true, false, true, false, true, false, true, false, true, false, true, true,
-                  false, true, false, true, false, true, false, true, false, true, false, true,
-                  true, false, true, false, true, false, true, false, true, false, true, false,
-                  true, true, false, true, false, true, false, true, false, true, false, true,
-                  false, true,
-                }));
-        JSONObject sub = new JSONObject();
-        sub.put("sub_bool", true);
-        sub.put("sub_int", 12);
-        sub.put("sub_string", "Test Test Test");
-        object.put("test_subs", new JSONArray(new JSONObject[] {sub, sub, sub, sub, sub, sub}));
-        break;
+        // TODO(jstocklass): make a complex object
       default:
         break;
     }
@@ -158,6 +94,7 @@ public class ITBigQueryStorageLongRunningWriteTest {
     dataset = RemoteBigQueryHelper.generateDatasetName();
     DatasetInfo datasetInfo =
         DatasetInfo.newBuilder(/* datasetId = */ dataset).setDescription(DESCRIPTION).build();
+    LOG.info("Creating dataset: " + dataset);
     bigquery.create(datasetInfo);
   }
 
@@ -173,7 +110,7 @@ public class ITBigQueryStorageLongRunningWriteTest {
   }
 
   @Test
-  public void testDefaultStream()
+  public void testSimpleDefaultStream()
       throws IOException, InterruptedException, ExecutionException,
           Descriptors.DescriptorValidationException {
     // TODO(jstocklass): Set up a default stream. Write to it for a long time,
@@ -202,39 +139,7 @@ public class ITBigQueryStorageLongRunningWriteTest {
             .build();
     bigquery.create(tableInfo);
 
-    StandardSQLTypeName[] array = new StandardSQLTypeName[] {StandardSQLTypeName.INT64};
-    String complexTableName = "JsonComplexTableDefaultStream";
-    TableInfo tableInfo2 =
-        TableInfo.newBuilder(
-                TableId.of(dataset, complexTableName),
-                StandardTableDefinition.of(
-                    Schema.of(
-                        Field.newBuilder("test_str", StandardSQLTypeName.STRING).build(),
-                        Field.newBuilder("test_numerics1", StandardSQLTypeName.NUMERIC)
-                            .setMode(Mode.REPEATED)
-                            .build(),
-                        Field.newBuilder("test_numerics2", StandardSQLTypeName.NUMERIC)
-                            .setMode(Mode.REPEATED)
-                            .build(),
-                        Field.newBuilder("test_numerics3", StandardSQLTypeName.NUMERIC)
-                            .setMode(Mode.REPEATED)
-                            .build(),
-                        Field.newBuilder("test_datetime", StandardSQLTypeName.DATETIME).build(),
-                        Field.newBuilder("test_bools", StandardSQLTypeName.BOOL)
-                            .setMode(Mode.REPEATED)
-                            .build(),
-                        Field.newBuilder(
-                                "test_subs",
-                                StandardSQLTypeName.STRUCT,
-                                Field.of("sub_bool", StandardSQLTypeName.BOOL),
-                                Field.of("sub_int", StandardSQLTypeName.INT64),
-                                Field.of("sub_string", StandardSQLTypeName.STRING))
-                            .setMode(Mode.REPEATED)
-                            .build())))
-            .build();
-    bigquery.create(tableInfo2);
-
-    int requestLimit = 1000;
+    int requestLimit = 10;
     long averageLatency = 0;
     long totalLatency = 0;
     TableName parent = TableName.of(ServiceOptions.getDefaultProjectId(), dataset, tableName);
@@ -269,37 +174,6 @@ public class ITBigQueryStorageLongRunningWriteTest {
         assertTrue(iter.hasNext());
         currentRow = iter.next();
         assertEquals("aaa", currentRow.get(0).getStringValue());
-      }
-      assertEquals(false, iter.hasNext());
-    }
-    parent = TableName.of(ServiceOptions.getDefaultProjectId(), dataset, complexTableName);
-    try (JsonStreamWriter jsonStreamWriter =
-        JsonStreamWriter.newBuilder(parent.toString(), tableInfo2.getDefinition().getSchema())
-            .createDefaultStream()
-            .build()) {
-      for (int i = 0; i < requestLimit; i++) {
-        JSONObject row = MakeJsonObject(RowComplexity.COMPLEX);
-        JSONArray jsonArr = new JSONArray(new JSONObject[] {row});
-        long startTime = System.nanoTime();
-        // TODO(jstocklass): Make asynchronized calls instead of synchronized calls
-        ApiFuture<AppendRowsResponse> response = jsonStreamWriter.append(jsonArr, -1);
-        long finishTime = System.nanoTime();
-        Assert.assertFalse(response.get().getAppendResult().hasOffset());
-        if (i != 0) {
-          totalLatency += (finishTime - startTime);
-        }
-      }
-      averageLatency = totalLatency / requestLimit;
-      LOG.info("Complex average Latency: " + String.valueOf(averageLatency) + " ns");
-      TableResult result2 =
-          bigquery.listTableData(
-              tableInfo2.getTableId(), BigQuery.TableDataListOption.startIndex(0L));
-      Iterator<FieldValueList> iter = result2.getValues().iterator();
-      FieldValueList currentRow2;
-      for (int i = 0; i < requestLimit; i++) {
-        assertTrue(iter.hasNext());
-        currentRow2 = iter.next();
-        assertEquals("aaa", currentRow2.get(0).getStringValue());
       }
       assertEquals(false, iter.hasNext());
     }
