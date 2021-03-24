a56f8b51870651375adaf4e59867cc06bd15b699
JacobStocklass <jstocklass@google.com>
2021-03-24 18:15:36 +0000

    Placeholder functions and tests for BigDecimal to String conversion

---
+++ google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta2/BigDecimalToStringConverterTest.java
@@ -0,0 +1,20 @@
+package com.google.cloud.bigquery.storage.v1beta2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class BigDecimalToStringConverterTest {
+
+  @Test
+  public void StringToBDTest() {
+    BigDecimalToStringConverter.StringToBigDecimal("100000");
+  }
+
+  @Test
+  public void BDToStringTest() {
+
+  }
+}
+
