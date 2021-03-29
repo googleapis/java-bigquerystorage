e6d4ed4e9295b86a2dd30158328e76085cc89b34
JacobStocklass <jstocklass@google.com>
2021-03-29 18:33:47 +0000

    Adding BigDecimal ByteString tests, currently not working

---
+++ google-cloud-bigquerystorage/src/test/java/com/google/cloud/bigquery/storage/v1beta2/BigDecimalByteStringEncoderTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.bigquery.storage.v1beta2;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.protobuf.ByteString;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class BigDecimalByteStringEncoderTest {
+  private static String MAX_BIGNUMERIC_VALUE_STRING =
+      "578960446186580977117854925043439539266.34992332820282019728792003956564819967";
+  private static String MIN_BIGNUMERIC_VALUE_STRING =
+      "-578960446186580977117854925043439539266.34992332820282019728792003956564819968";
+  @Test
+  public void testEncodeToBigDecimal(){
+    List<ByteString> valueByteStrings = Arrays.asList(
+        ByteString.copyFrom(new byte[] {(byte) 0x0}),
+        ByteString.copyFrom(new byte[] {(byte) 0x12}),
+        ByteString.copyFrom(new byte[] {(byte) 0x22}),
+        ByteString.copyFrom(new byte[] {(byte) 0x99999999}),
+        ByteString.copyFrom(new byte[] {(byte) 0x99999999}));
+    List<BigDecimal> bigDecimals = Arrays.asList(
+        BigDecimal.valueOf(0),
+        BigDecimal.valueOf(1.2),
+        BigDecimal.valueOf(-1.2),
+        BigDecimal.valueOf(578960446186580977117854925043439539266.34992332820282019728792003956564819967),
+        BigDecimal.valueOf(-578960446186580977117854925043439539266.34992332820282019728792003956564819967));
+    int iter = 0;
+    for (ByteString check : valueByteStrings) {
+      ByteString value = BigDecimalByteStringEncoder.encodeToByteString(bigDecimals.get(iter));
+      assertThat(check.equals(value));
+      iter++;
+    }
+    // TODO(jstocklass): How do i make this fail.... I don't trust this test at all
+
+  }
+  public void testEncodeToByteString(){
+    // start with some bigdecimals
+    // convert to bytestrings
+    // make sure it's correct
+  }
+}
