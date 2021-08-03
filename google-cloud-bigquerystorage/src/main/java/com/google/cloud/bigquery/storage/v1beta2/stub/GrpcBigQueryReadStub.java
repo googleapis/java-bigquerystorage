1e86751fe40a992ea8c64e7190951a93f2f69019
yoshi-automation <yoshi-automation@google.com>
2021-08-03 14:50:07 -0700

    chore: release gapic-generator-java v1.0.17
    Committer: @miraleung
    PiperOrigin-RevId: 388499329
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Aug 3 11:12:29 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bb0a090d9204110042ab5dee2ce9e06e8071ce54
    Source-Link: https://github.com/googleapis/googleapis/commit/bb0a090d9204110042ab5dee2ce9e06e8071ce54

--- google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1beta2/stub/GrpcBigQueryReadStub.java
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1beta2/stub/GrpcBigQueryReadStub.java
@@ -207,7 +207,13 @@ public class GrpcBigQueryReadStub extends BigQueryReadStub {
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
