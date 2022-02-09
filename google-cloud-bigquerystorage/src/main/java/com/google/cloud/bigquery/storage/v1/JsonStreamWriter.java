dee703ea3b774dd7831693f28606c9f348a6b740
yirutang <yiru@google.com>
2022-02-09 14:29:30 -0800

    .

--- google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1/JsonStreamWriter.java
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1/JsonStreamWriter.java
@@ -405,10 +405,10 @@ public class JsonStreamWriter implements AutoCloseable {
     }
 
     /**
-     * @Deprecated
-     * Setter for a reconnectAfter10M, temporaily workaround for omg/48020. Fix for the omg is
-     * supposed to roll out by 2/11/2022 Friday. If you set this to True, your write will be slower
-     * (0.75MB/s per connection), but your writes will not be stuck as a sympton of omg/48020.
+     * @Deprecated Setter for a reconnectAfter10M, temporaily workaround for omg/48020. Fix for the
+     * omg is supposed to roll out by 2/11/2022 Friday. If you set this to True, your write will be
+     * slower (0.75MB/s per connection), but your writes will not be stuck as a sympton of
+     * omg/48020.
      *
      * @param reconnectAfter10M
      * @return Builder
