fe273b5a97591e9a0622b450b51d0c55c80a9cdf
yirutang <yiru@google.com>
2022-07-26 09:51:35 -0700

    .

---
+++ samples/snippets/src/main/java/com/example/bigquerystorage/WriteToBufferedStream.java
@@ -0,0 +1,261 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bigquerystorage;
+
+// [START bigquerystorage_jsonstreamwriter_pending]
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.cloud.bigquery.storage.v1.*;
+import com.google.cloud.bigquery.storage.v1.BigQueryWriteClient;
+import com.google.cloud.bigquery.storage.v1.Exceptions.StorageException;
+import com.google.cloud.bigquery.storage.v1.JsonStreamWriter;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.protobuf.Descriptors.DescriptorValidationException;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.concurrent.*;
+import javax.annotation.concurrent.GuardedBy;
+
+import com.google.protobuf.Int64Value;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class WriteBufferedStream {
+
+	public static void runWritePendingStream()
+			throws DescriptorValidationException, InterruptedException, IOException {
+		// TODO(developer): Replace these variables before running the sample.
+		String projectId = "MY_PROJECT_ID";
+		String datasetName = "MY_DATASET_NAME";
+		String tableName = "MY_TABLE_NAME";
+
+		writeBufferedStream(projectId, datasetName, tableName);
+	}
+
+	public static void writeBufferedStream(String projectId, String datasetName, String tableName)
+			throws DescriptorValidationException, InterruptedException, IOException {
+		BigQueryWriteClient client = BigQueryWriteClient.create();
+		TableName parentTable = TableName.of(projectId, datasetName, tableName);
+
+		DataWriter writer = new DataWriter();
+		// One time initialization.
+		writer.initialize(parentTable, client);
+
+		try {
+			// Write two batches of fake data to the stream, each with 10 JSON records.  Data may be
+			// batched up to the maximum request size:
+			// https://cloud.google.com/bigquery/quotas#write-api-limits
+			int offset = 0;
+			for (int i = 0; i < 2; i++) {
+				// Create a JSON object that is compatible with the table schema.
+				JSONArray jsonArr = new JSONArray();
+				for (int j = 0; j < 10; j++) {
+					JSONObject record = new JSONObject();
+					record.put("col1", String.format("batch-record %03d-%03d", i, j));
+					jsonArr.put(record);
+				}
+				writer.append(jsonArr, -1);
+				offset += jsonArr.length();
+			}
+		} catch (ExecutionException e) {
+			// If the wrapped exception is a StatusRuntimeException, check the state of the operation.
+			// If the state is INTERNAL, CANCELLED, or ABORTED, you can retry. For more information, see:
+			// https://grpc.github.io/grpc-java/javadoc/io/grpc/StatusRuntimeException.html
+			System.out.println("Failed to append records. \n" + e);
+		}
+
+		// Final cleanup for the stream.
+		writer.cleanup(client);
+		System.out.println("Appended records successfully.");
+	}
+
+	// A simple wrapper object showing how the stateful stream writer should be used.
+	private static class DataWriter {
+
+		private JsonStreamWriter streamWriter;
+		// Track the number of in-flight requests to wait for all responses before shutting down.
+		private final Phaser inflightRequestCount = new Phaser(1);
+		// A queue that contains the list of offsets need to be flushed.
+		private final BlockingQueue<Integer> flushQueue = new LinkedBlockingDeque<>(10 * 1000);
+		// A separate thread that handles flush calls.
+		private Thread flushThread;
+
+		private final Object lock = new Object();
+		Condition hasMessageInFlushQueue = lock.newCondition();
+
+		@GuardedBy("lock")
+		private RuntimeException error = null;
+
+		void initialize(TableName parentTable, BigQueryWriteClient client)
+				throws IOException, DescriptorValidationException, InterruptedException {
+			// Initialize a write stream for the specified table.
+			// For more information on WriteStream.Type, see:
+			// https://googleapis.dev/java/google-cloud-bigquerystorage/latest/com/google/cloud/bigquery/storage/v1/WriteStream.Type.html
+			WriteStream stream = WriteStream.newBuilder().setType(WriteStream.Type.BUFFERED).build();
+
+			CreateWriteStreamRequest createWriteStreamRequest =
+					CreateWriteStreamRequest.newBuilder()
+							.setParent(parentTable.toString())
+							.setWriteStream(stream)
+							.build();
+			WriteStream writeStream = client.createWriteStream(createWriteStreamRequest);
+
+			// Use the JSON stream writer to send records in JSON format.
+			// For more information about JsonStreamWriter, see:
+			// https://googleapis.dev/java/google-cloud-bigquerystorage/latest/com/google/cloud/bigquery/storage/v1beta2/JsonStreamWriter.html
+			streamWriter =
+					JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema()).build();
+
+			// Start flush queue.
+			this.flushThread =
+					new Thread(
+							new Runnable() {
+								@Override
+								public void run() {
+									flushLoop();
+								}
+							});
+			this.flushThread.start();
+		}
+
+		public void append(JSONArray data, long offset)
+				throws DescriptorValidationException, IOException, ExecutionException {
+			synchronized (this.lock) {
+				// If earlier appends have failed, we need to reset before continuing.
+				if (this.error != null) {
+					throw this.error;
+				}
+			}
+			// Append asynchronously for increased throughput.
+			ApiFuture<AppendRowsResponse> future = streamWriter.append(data, offset);
+			ApiFutures.addCallback(
+					future, new AppendCompleteCallback(this), MoreExecutors.directExecutor());
+			// Increase the count of in-flight requests.
+			inflightRequestCount.register();
+		}
+
+		/*
+		 * This loop is executed in a separate thread.
+		 *
+		 * It takes requests from waiting queue and sends them to server.
+		 */
+		private void flushLoop() {
+			while (!waitingFlushQueueDrained()) {
+				long flushOffset = 0;
+				this.lock.lock();
+				try {
+					hasMessageInWaitingQueue.await(100, TimeUnit.MILLISECONDS);
+					Integer flushOffset = -1;
+					while (!this.flushQueue.isEmpty()) {
+						flushOffset = this.waitingRequestQueue.pollFirst();
+					}
+				} catch (InterruptedException e) {
+					log.warning(
+							"Interrupted while waiting for message. Stream: Error: "
+									+ e.toString());
+				} finally {
+					this.lock.unlock();
+				}
+
+				if (flushOffset >= 0) {
+					FlushRowsRequest preparedRequest = FlushRowsRequest.newBuilder().setWriteStream(streamName).setOffset(
+							Int64Value.newBuilder().setValue(flushOffset).build();
+				}
+			}
+		}
+
+		/*
+		 * Returns true if waiting queue is drain, a.k.a. no more requests in the waiting queue.
+		 *
+		 * It serves as a signal to append thread that there cannot be any more requests in the waiting
+		 * queue and it can prepare to stop.
+		 */
+		private boolean waitingFlushQueueDrained() {
+			this.lock.lock();
+			try {
+				return (this.userClosed || this.connectionFinalStatus != null)
+						       && this.waitingRequestQueue.isEmpty();
+			} finally {
+				this.lock.unlock();
+			}
+		}
+
+		public void cleanup(BigQueryWriteClient client) {
+			// Wait for all in-flight requests to complete.
+			inflightRequestCount.arriveAndAwaitAdvance();
+
+			// Close the connection to the server.
+			streamWriter.close();
+
+			// Verify that no error occurred in the stream.
+			synchronized (this.lock) {
+				if (this.error != null) {
+					throw this.error;
+				}
+			}
+
+			// Finalize the stream.
+			FinalizeWriteStreamResponse finalizeResponse =
+					client.finalizeWriteStream(streamWriter.getStreamName());
+			System.out.println("Rows written: " + finalizeResponse.getRowCount());
+		}
+
+		public String getStreamName() {
+			return streamWriter.getStreamName();
+		}
+
+		static class AppendCompleteCallback implements ApiFutureCallback<AppendRowsResponse> {
+
+			private final DataWriter parent;
+
+			public AppendCompleteCallback(DataWriter parent) {
+				this.parent = parent;
+			}
+
+			public void onSuccess(AppendRowsResponse response) {
+				System.out.format("Append %d success\n", response.getAppendResult().getOffset().getValue());
+				if (response.getAppendResult().getOffset() == parent.getFlushOffsets()) {
+					parent.flushQuque.write();
+				}
+				done();
+			}
+
+			public void onFailure(Throwable throwable) {
+				synchronized (this.parent.lock) {
+					if (this.parent.error == null) {
+						StorageException storageException = Exceptions.toStorageException(throwable);
+						this.parent.error =
+								(storageException != null) ? storageException : new RuntimeException(throwable);
+					}
+				}
+				System.out.format("Error: %s\n", throwable.toString());
+				done();
+			}
+
+			private void done() {
+				// Reduce the count of in-flight requests.
+				this.parent.inflightRequestCount.arriveAndDeregister();
+			}
+		}
+	}
+}
+// [END bigquerystorage_jsonstreamwriter_pending]
