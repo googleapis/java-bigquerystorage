dc44442f7a48b8afc8a9588c3ba0825845e425f5
Yiru Tang <yiru@google.com>
2022-12-03 00:51:15 +0000

    .

--- google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1/SimpleWriter.java
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1/SimpleWriter.java
@@ -2,56 +2,74 @@ package com.google.cloud.bigquery.storage.v1;
 
 import com.google.api.core.ApiFuture;
 import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.cache.RemovalListener;
 import com.google.common.cache.RemovalNotification;
 import com.google.protobuf.Descriptors.DescriptorValidationException;
 import java.io.IOException;
-import com.google.common.cache.LoadingCache;
-import com.google.common.cache.CacheBuilder;
-
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 import org.json.JSONArray;
 
 /**
- * A SimpleWriter provides a way to write to BigQuery without a Stream. 
+ * A SimpleWriter provides a way to write to BigQuery without a Stream. Underneath, it maintains a
+ * cache of JsonStreamWriter based on talbe name.
  */
 public class SimpleWriter {
   BigQueryWriteClient client;
   LoadingCache<String, JsonStreamWriter> writerCache;
+  String traceId;
 
-  /**
-   * Creates a SimpleWriter instance with provided BigQueryWriteClient. This object should be
-   * reused to do as many append calls as possible.
-   * @param client
-   * @throws IOException
-   */
-  public SimpleWriter(BigQueryWriteClient client) throws IOException {
-    new SimpleWriter(client, "maximumSize=100,expireAfterWrite=60m");
+  /** Constructs a new {@link SimpleWriter.Builder} using the given bigquery write client. */
+  public static SimpleWriter.Builder newBuilder(BigQueryWriteClient client) {
+    return new SimpleWriter.Builder(client);
   }
 
-  /**
-   * Creates a SimpleWriter with custom caching policy for JsonStreamWriter.
-   * @param client
-   * @param cacheSpec
-   * @throws IOException
-   */
-  public SimpleWriter(BigQueryWriteClient client, String cacheSpec) throws IOException {
+  private SimpleWriter(SimpleWriter.Builder builder) throws IOException {
+    this.client = builder.client;
+    this.traceId = builder.traceId;
+    this.writerCache =
+        CacheBuilder.from(builder.cacheSpec)
+            .removalListener(
+                new RemovalListener<String, JsonStreamWriter>() {
+                  public void onRemoval(
+                      RemovalNotification<String, JsonStreamWriter> notification) {
+                    notification.getValue().close();
+                  }
+                })
+            .build(
+                new CacheLoader<String, JsonStreamWriter>() {
+                  public JsonStreamWriter load(String key)
+                      throws DescriptorValidationException, IOException, InterruptedException {
+                    return JsonStreamWriter.newBuilder(key, client)
+                        .setEnableConnectionPool(true)
+                        .setTraceId(traceId)
+                        .build();
+                  }
+                });
+  }
+
+  public static final class Builder {
+    BigQueryWriteClient client;
+    String cacheSpec = "maximumSize=100,expireAfterWrite=60m";
+    String traceId = "SimpleWriter:null";
+
+    private Builder(BigQueryWriteClient client) {
       this.client = Preconditions.checkNotNull(client);
-      LoadingCache<String, JsonStreamWriter> writerCache = CacheBuilder.from(cacheSpec)
-          .removalListener(new RemovalListener<String, JsonStreamWriter>(){
-            public void onRemoval(RemovalNotification<String, JsonStreamWriter> notification) {
-              notification.getValue().close();
-            }})
-          .build(
-              new CacheLoader<String, JsonStreamWriter>() {
-                public JsonStreamWriter load(String key)
-                    throws DescriptorValidationException, IOException, InterruptedException {
-                  return JsonStreamWriter.newBuilder(key, client)
-                      .setEnableConnectionPool(true).setTraceId("SimpleWrite").build();
-                }
-              });
+    }
+
+    /** CacheSpec for the JsonStreamWriter cache. */
+    public SimpleWriter.Builder setCacheSpec(String cacheSpec) {
+      this.cacheSpec = cacheSpec;
+      return this;
+    }
+
+    /** One time trace id to apply to all writes */
+    public SimpleWriter.Builder setTraceId(String traceId) {
+      this.traceId = "SimpleWriter_" + traceId;
+      return this;
+    }
   }
 
   /**
