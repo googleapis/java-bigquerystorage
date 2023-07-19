ee863155d8387ff5759f5a9fb869b186e1626516
Yiru Tang <yiru@google.com>
2023-07-19 21:50:30 +0000

    .

--- google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1/SchemaAwareStreamWriter.java
+++ google-cloud-bigquerystorage/src/main/java/com/google/cloud/bigquery/storage/v1/SchemaAwareStreamWriter.java
@@ -44,12 +44,12 @@ import javax.annotation.Nullable;
  * the BigQuery table schema is updated, users will be able to ingest data on the new schema after
  * some time (in order of minutes).
  *
- * NOTE: The schema update ability will be disabled when you pass in a table schema explicitly
+ * <p>NOTE: The schema update ability will be disabled when you pass in a table schema explicitly
  * through the writer. It is recommended that user either use JsonStreamWriter (which fully manages
- * table schema) or StreamWriter (which accepts proto format in raw and user will handle the
- * schema update event themsevles). If you sse this class, you need to be very cautious about
- * possible mistmach between the writer's schema and the input data, any mismatch of the two will
- * cause data corruption.
+ * table schema) or StreamWriter (which accepts proto format in raw and user will handle the schema
+ * update event themsevles). If you sse this class, you need to be very cautious about possible
+ * mistmach between the writer's schema and the input data, any mismatch of the two will cause data
+ * corruption.
  */
 public class SchemaAwareStreamWriter<T> implements AutoCloseable {
   private static final Logger LOG = Logger.getLogger(SchemaAwareStreamWriter.class.getName());
