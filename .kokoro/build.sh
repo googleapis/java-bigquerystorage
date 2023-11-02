231be555c58b4259419e83dc3e7d174dbffb4f24
Neenu1995 <neenushaji@google.com>
2023-11-02 10:50:38 -0400

    delimit exclamation

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -62,7 +62,7 @@ javadoc)
 integration)
     mvn -B ${INTEGRATION_TEST_ARGS} \
       -Penable-integration-tests \
-      -Dtest=!ITCustomBigQueryDemoTest \
+      -Dtest=\!ITCustomBigQueryDemoTest \
       -DtrimStackTrace=false \
       -Dclirr.skip=true \
       -Denforcer.skip=true \
