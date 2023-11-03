dc0c554c91ff747f8f26bd19200e8b8139c915af
Farhan Ahmed <fahmz@google.com>
2023-11-03 11:08:05 -0400

    fix: checkstyle

--- samples/snippets/src/test/java/com/example/bigquerystorage/JsonWriterStreamCdcIT.java
+++ samples/snippets/src/test/java/com/example/bigquerystorage/JsonWriterStreamCdcIT.java
@@ -64,9 +64,9 @@ public class JsonWriterStreamCdcIT {
   @Test
   public void testJsonWriterStreamCdc() throws Exception {
     Path dataFilePath = System.getProperty("user.dir");
-        // FileSystems.getDefault().getPath(
-        // "/java-bigquerystorage/samples/snippets/src/test/resources",
-        // "NewCustomers.json");
+    // FileSystems.getDefault().getPath(
+    // "/java-bigquerystorage/samples/snippets/src/test/resources",
+    // "NewCustomers.json");
 
     System.out.println(dataFilePath.toString());
     String[] args = {GOOGLE_CLOUD_PROJECT, datasetName, "customers", dataFilePath.toString()};
