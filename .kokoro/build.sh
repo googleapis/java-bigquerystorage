f84670aa33487c70f2db473d5039af28e70600ff
Neenu1995 <neenushaji@google.com>
2023-11-06 16:02:17 -0500

    chore: test set up

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -65,7 +65,7 @@ integration)
       -DtrimStackTrace=false \
       -Dclirr.skip=true \
       -Denforcer.skip=true \
-      -Dtest=!ITBigQueryWriteQuotaRetryTest,!ITBigQueryWriteNonQuotaRetryTest \
+      "-Dtest=!ITBigQueryWriteQuotaRetryTest, !ITBigQueryWriteNonQuotaRetryTest" \
       -Dsurefire.failIfNoSpecifiedTests=false \
       -fae \
       verify
