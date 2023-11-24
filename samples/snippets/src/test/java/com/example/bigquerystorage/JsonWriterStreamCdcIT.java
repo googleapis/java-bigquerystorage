863798587988533476b82c1edd118f3d469202e4
Phong Chuong <147636638+PhongChuong@users.noreply.github.com>
2023-11-24 10:06:41 -0500

    fix json path issue

--- samples/snippets/src/test/java/com/example/bigquerystorage/JsonWriterStreamCdcIT.java
+++ samples/snippets/src/test/java/com/example/bigquerystorage/JsonWriterStreamCdcIT.java
@@ -64,8 +64,9 @@ public class JsonWriterStreamCdcIT {
   @Test
   public void testJsonWriterStreamCdc() throws Exception {
     Path dataFilePath = FileSystems.getDefault().getPath(
-        "src/test/resources", "NewCustomers.json");
-    String[] args = {GOOGLE_CLOUD_PROJECT, datasetName, "customers", dataFilePath.toString()};
+        "../snippets/src/test/resources", "NewCustomers.json");
+    String[] args =
+        {GOOGLE_CLOUD_PROJECT, datasetName, "customers", dataFilePath.toAbsolutePath().toString()};
 
     JsonWriterStreamCdc.main(args);
     assertThat(bout.toString()).contains("Wrote batch");
