6c088d70e079886cc59e0e35037244ed68519d2f
Yiru Tang <yiru@google.com>
2025-07-01 15:11:58 -0700

    fix syntax error

--- samples/snippets/src/main/java/com/example/bigquerystorage/WriteToDefaultStream.java
+++ samples/snippets/src/main/java/com/example/bigquerystorage/WriteToDefaultStream.java
@@ -184,7 +184,8 @@ public class WriteToDefaultStream {
                   .setChannelsPerCpu(2)
                   .build())
           .setEnableConnectionPool(true)
-          .setFlowControlSettings(FlowControlSettings.newBuilder().setMaxOutstandingElementCount(100).build());
+          .setFlowControlSettings(
+              FlowControlSettings.newBuilder().setMaxOutstandingElementCount(100).build())
           // If value is missing in json and there is a default value configured on bigquery
           // column, apply the default value to the missing value field.
           .setDefaultMissingValueInterpretation(
